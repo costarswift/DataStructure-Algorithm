70b2bcfa454a633bb1b8b611526e256387204bbe
costarswift <realcostarswift@gmail.com>
2023-03-22 21:35:12 +0800

    Leetcode 415 第二种解法

--- src/com/xaut/Dec_17_2022/Leetcode415.java
+++ src/com/xaut/Dec_17_2022/Leetcode415.java
@@ -20,4 +20,22 @@ public class Leetcode415 {
         }
         return ans.reverse().toString();
     }
+
+    public String addStrings2(String num1, String num2) {
+        StringBuilder ans = new StringBuilder();
+
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        while(i >= 0 || j >= 0 || carry != 0){
+            int sum = carry;
+            if(i >= 0) sum += num1.charAt(i--) - '0';
+            if(j >= 0) sum += num2.charAt(j--) - '0';
+            ans.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return ans.reverse().toString();
+    }
 }
