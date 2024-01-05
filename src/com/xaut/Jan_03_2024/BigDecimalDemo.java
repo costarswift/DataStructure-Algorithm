427829a63cee5db3cd70594cfa2987ce2c8bd3f1
Costar <costarswift@gmail.com>
2024-01-05 21:43:06 +0800

    BigDecimal second experience

--- src/com/xaut/Jan_03_2024/BigDecimalDemo.java
+++ src/com/xaut/Jan_03_2024/BigDecimalDemo.java
@@ -1,6 +1,7 @@
 package com.xaut.Jan_03_2024;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * @Author Costar Swift
@@ -8,28 +9,19 @@ import java.math.BigDecimal;
  */
 public class BigDecimalDemo {
     public static void main(String[] args) {
-        float a = 57.3f;
-        BigDecimal decimalA = new BigDecimal(a);
-        System.out.println(decimalA);
+        System.out.println(0.1 + 0.2);
+        System.out.println(tool("0.1", "0.2", 0.001D));
+        System.out.println(tool("0.1", "0.2", null));
+        System.out.println(tool("0.11", "0.21", 0.011));
+    }
 
-        double b = 57.3;
-
-        BigDecimal decimalB = new BigDecimal(b);
-        System.out.println(decimalB);
-
-        double c = 57.3;
-        BigDecimal decimalC = new BigDecimal(Double.toString(c));
-        System.out.println(decimalC);
-
-        double d = 57.3;
-        BigDecimal decimalD = BigDecimal.valueOf(d);
-        System.out.println(decimalD);
-
-        BigDecimal add = decimalC.add(decimalD);
-        System.out.println(add);
-
-        BigDecimal subtract = decimalC.subtract(decimalD);
-        System.out.println(subtract);
-        System.out.println(subtract.doubleValue());
+    private static Double tool(String var1, String var2, Double var3) {
+        BigDecimal totalNetAmount = new BigDecimal(var1);
+        if (var3 != null) {
+            totalNetAmount = totalNetAmount.add(new BigDecimal(var2)).subtract(BigDecimal.valueOf(var3));
+        } else {
+            totalNetAmount = totalNetAmount.add(new BigDecimal(var2));
+        }
+        return totalNetAmount.setScale(6, RoundingMode.UP).doubleValue();
     }
 }
